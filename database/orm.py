bbece64ff9c1b1e38bb951b8fde4de4a8b997d18
Luis Zermeno <lgzg90@hotmail.com>
2026-02-05 17:55:15 +0100

    feature: new table in database for the analysis the llms do for financials

--- database/orm.py
+++ database/orm.py
@@ -1,8 +1,9 @@
-from sqlalchemy import Column, Integer, String, DateTime, Text
+from sqlalchemy import Column, Integer, String, DateTime, Date, Text
 from sqlalchemy.sql import func
 from sqlalchemy.orm import declarative_base
 from pgvector.sqlalchemy import Vector
 
+from sqlalchemy.dialects.postgresql import JSONB
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker 
@@ -28,7 +29,16 @@ class DocumentChunk(Base):
     content = Column(Text, nullable=False)
     embedding = Column(Vector(384))
     ticker = Column(String(10), nullable=False)
-    filing_date = Column(DateTime)
+    filing_date = Column(Date)
     year = Column(Integer)
     quarter = Column(String(2))
 
+class LLM_Financial_Analysis(Base):
+    __tablename__='llm_financial_analysis'
+
+    id = Column(Integer, primary_key=True)
+    ticker = Column(String(10), nullable=False)
+    llm_model = Column(String(100), nullable=False)
+    analysis = Column(JSONB)
+    latest_filling_date = Column(Date)
+    created_at = Column(DateTime, default=func.now())
