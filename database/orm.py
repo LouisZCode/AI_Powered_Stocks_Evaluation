60a1a748fd74f3ed5b5191359398a83f749ae37b
Luis Zermeno <lgzg90@hotmail.com>
2026-02-21 12:17:59 +0100

    feature: add table for oauth in prep for oauth google and github

--- database/orm.py
+++ database/orm.py
@@ -1,12 +1,15 @@
-from sqlalchemy import Column, Integer, String, DateTime, Date, Text, UniqueConstraint
+import uuid
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, DateTime, Date, Text, UniqueConstraint
 from sqlalchemy.sql import func
 from sqlalchemy.orm import declarative_base
 from pgvector.sqlalchemy import Vector
 
+from sqlalchemy.dialects.postgresql import UUID
+
 from sqlalchemy.dialects.postgresql import JSONB
 
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker 
+from sqlalchemy.orm import sessionmaker, relationship
 from config import DB_URL
 
 engine = create_engine(DB_URL)
@@ -20,6 +23,49 @@ def get_db():
         db.close()
 
 Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    name = Column(String(100), nullable=False)
+    email = Column(String(200), nullable=False, unique=True)
+    tier = Column(String(20), nullable=False, default="free")
+    creation_date = Column(DateTime, default=func.now())
+    update_tier_date = Column(Date, default=func.now())
+
+    activities = relationship("Activity", back_populates="user")
+    oauth_accounts = relationship("OauthProvider", back_populates="user", cascade="all, delete-orphan")
+
+
+class Activity(Base):
+    __tablename__ = "activities"
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(UUID(as_uuid=True), ForeignKey('users.id'), nullable=False)
+    activity_name = Column(String(50), nullable=False)
+    ticker = Column(String(10), nullable=False)
+    fast_use = Column(Integer)
+    deep_use = Column(Integer)
+    debate_level = Column(Integer)
+    activity_start_date = Column(DateTime, default=func.now())
+
+    user = relationship("User", back_populates="activities")
+
+class OauthProvider(Base):
+    __tablename__ = "oauth_accounts"
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(UUID(as_uuid=True), ForeignKey('users.id'), nullable=False)
+    provider = Column(String(100), nullable=False)
+    provider_user_id = Column(String(100), nullable=False)
+    created_at = Column(DateTime, default=func.now())
+
+    user = relationship("User", back_populates="oauth_accounts")
+
+    __table_args__ = (UniqueConstraint("provider", "provider_user_id", name="uq_provider_account"))
+
+
 
 class DocumentChunk(Base):
     __tablename__ = 'chunks'
