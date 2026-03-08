d74770507f05dcaad60016aad5bb8c1516c7d952
Luis Zermeno <lgzg90@hotmail.com>
2026-03-08 11:30:51 +0100

    fix:redirect from www to / for agorafinancials.com

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [{ type: "host", value: "www.agorafinancials.com" }],
+        destination: "https://agorafinancials.com/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
